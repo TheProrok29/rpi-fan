1b84e3d74571568269a0cd279fa4c781248a2c4c
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-06-11 10:36:16 +0200

    Add main script loop

--- fan.py
+++ fan.py
@@ -1,11 +1,34 @@
 import subprocess
 import time
 
-from gpiozero import LED, OutputDevice
+from gpiozero import LED, CPUTemperature, OutputDevice
 
-# Config
+#                     Config
+
 FAN = LED(18)  # GPIO pin using to control the fan.
 SLEEP_INTERVAL = 5  # (seconds) How often we check the core temperature.
 ON_TEMP = 65  # (degrees Celsius) Temperature to start fan.
 OFF_TEMP = 45  # (degress Celsius) Temperature to stop fan.
 
+CPU = CPUTemperature()
+
+
+def get_temp() -> float:
+    """
+    Return the core temperature using gpiozero.
+    """
+    return CPU.temperature
+
+
+if __name__ == '__main__':
+
+    if OFF_TEMP >= ON_TEMP:
+        raise RuntimeError('OFF_THRESHOLD must be less than ON_THRESHOLD')
+
+    while True:
+        temp = get_temp()
+        if temp > ON_TEMP and not FAN.value:
+            FAN.on()
+        elif FAN.value and temp < OFF_TEMP:
+            FAN.off()
+        time.sleep(SLEEP_INTERVAL)
